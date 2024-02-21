0a431fff59677d82334188ab067bb4285b6b7da2
NguyenDucDuy_63133799 <nguyenducduymmyn@gmail.com>
2024-02-21 12:01:15 +0700

    lab1

---
+++ Lab1/src/Bai1.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+public class Bai1 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.println("Ho ten sinh vien");
+		String ten = scanner.nextLine();
+		System.out.println("Hay nhap diem tb");
+		double diemTB = scanner.nextDouble();
+		
+		scanner.close();
+	}
+
+}
---
+++ Lab1/src/Bai3.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Bai3 {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		System.out.print("Hay nhap mot canh: ");
+		int canh = scanner.nextInt();
+		System.out.printf("The tich la:"+ Math.pow(canh,3));
+
+	}
+
+}
